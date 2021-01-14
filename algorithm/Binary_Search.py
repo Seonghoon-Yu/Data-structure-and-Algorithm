950fdfd41889e2d9761f1e5a84d49c3100021d1d
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-01-14 21:00:40 +0900

    Create Binary_Search.py

---
+++ algorithm/Binary_Search.py
@@ -0,0 +1,30 @@
+# 재귀 구현
+def search(nums, target):
+    def binary_search(left, right):
+        if left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] < target:
+                return binary_search(mid + 1, right)
+            elif nums[mid] > target:
+                return binary_search(left, mid - 1)
+            else:
+                return mid
+        else:
+            return -1
+    return binary_search(0, len(nums)-1)
+
+
+# 반복 구현
+def search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+
+        if nums[mid] < target:
+            left = mid + 1
+        elif nums[mid] > target:
+            right = mid - 1
+        else:
+            return mid
+    return -1
