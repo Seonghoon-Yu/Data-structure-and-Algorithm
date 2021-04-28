b1cc9cf8b268127a6107c14d01d63bd1117f1083
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-04-29 01:05:57 +0900

    Update insertion_sort.py

--- algorithm/insertion_sort.py
+++ algorithm/insertion_sort.py
@@ -1,8 +1,8 @@
-def insert_sort(A): 
-    for i in range(1, len(A)):
-        insert = A[i] # insert 값 지정
-        j = i # 비교를 위해 j 지정
-        while j > 0 and A[j-1] > insert: # j가 0보다 크고, j-1이 insert보다 크면 실행
-            A[j] = A[j-1] # A[j]를 A[j-1]로 shift 하여 공간 확보
-            j -= 1        # j값 갱신
-        A[j] = insert     # [j-1]보다 크면 [j]값에 insert 값 지정
+def insert(arr):
+    for j in range(1, len(arr)):
+        key = arr[j]
+        i = j-1
+        while i >= 0 and arr[i] > key:
+            arr[i+1] = arr[i]
+            i -= 1
+        arr[i+1] = arr[j]
