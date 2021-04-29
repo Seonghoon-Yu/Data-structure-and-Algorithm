f2272eb490a940b7bea8b71647987acf7f9a6cc6
Seonghoon-Yu <75726938+Seonghoon-Yu@users.noreply.github.com>
2021-04-29 14:10:52 +0900

    Update quick_sort.py

--- algorithm/quick_sort.py
+++ algorithm/quick_sort.py
@@ -1,15 +1,17 @@
-def quick_sort(A, lo, hi): # A: 배열, lo: 시작 인덱스, hi: 마지막 인덱스
-    # 파티션 분할 함수 정의
-    def partition(lo, hi):
-        pivot = A[hi] # 오른쪽 맨 끝을 pivot으로 선정
-        left = lo
-        
-        for right in range(lo, hi):
-            if A[right] < pivot: # right가 pivot보다 작으면 left와 right 값 변경
-                A[right], A[left] = A[left], A[right]
-                left += 1 # left 1칸 이동
-        A[left], A[hi] = A[hi], A[left] # left와 pivot 위치 변경
-        return left
-    pivot = partition(lo, hi)
-    quick_sort(A, lo, pivot - 1)
-    quick_sort(A, pivot + 1, hi)
+def quick_sort(arr,L,R):
+    if len(arr) <= 1:
+        return arr
+
+    def partition(L, R):
+        pivot = arr[R]
+        i = L-1
+        for j in range(L, R):
+            if arr[j] < pivot:
+                i += 1
+                arr[i], arr[j] = arr[j], arr[i]
+        arr[i+1], arr[R] = arr[R], arr[i+1]
+        return i+1
+    if L < R: # 중지 조건
+        pivot = partition(L,R)
+        quick_sort(arr, L, pivot-1)
+        quick_sort(arr, pivot+1, R)
